240ac451ebfe312d642b22056aa0255a1cd049b3
Tarodictrl <vudi600@gmail.com>
2022-12-17 02:10:57 +0700

    Add lab4

--- js/time.js
+++
@@ -1,8 +0,0 @@
-function muchTimePassed() {
-    var d = new Date, dt = d.getTime(); d.setMonth(8, 1);
-    if (d.getTime() > dt) d.setFullYear(d.getFullYear() - 1);
-    var n = Math.floor((dt - d.getTime()) / 8.64e7 / 7);
-    console.log('С 1 сентября ' + d.getFullYear() + ' года прошло ' + n + ' недель.');
-}
-
-muchTimePassed();
